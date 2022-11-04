534353dd13d425bcb5fafc6c779c8ab51f34404b
galopianciola <galoepianciolab@gmail.com>
2022-11-04 13:26:56 -0300

    Incluyo router en urls api

--- polititweet/api/urls.py
+++ polititweet/api/urls.py
@@ -1,7 +1,6 @@
 
 from django.conf.urls import include
 from django.urls import path
-from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 from . import viewsets, views
@@ -11,10 +10,6 @@ router = DefaultRouter()
 router.register('deleted-tweets', viewsets.DeletedTweetsViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('politicians/', views.UsersView.as_view(), name='politicians')
 ]
-
-if settings.DEBUG:
-    urlpatterns = [
-        path('', include(router.urls))
-    ] + urlpatterns
