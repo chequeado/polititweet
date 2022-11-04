f0abcd41bbca74b2b9d32addcdaffacd1cae57f4
galopianciola <galoepianciolab@gmail.com>
2022-11-04 13:31:57 -0300

    Elimino root de la api cambiando a SimpleRouter

--- polititweet/api/urls.py
+++ polititweet/api/urls.py
@@ -1,12 +1,12 @@
 
 from django.conf.urls import include
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from . import viewsets, views
 
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register('deleted-tweets', viewsets.DeletedTweetsViewSet)
 
 urlpatterns = [
